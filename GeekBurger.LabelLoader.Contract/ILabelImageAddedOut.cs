d716e3e288e95d3abb108d38c9f7afa2e8d7a02e
Fernando Albuquerque <fer.albuquerque90@gmail.com>
2018-03-12 21:58:16 -0300

    Inclusão do projeto de contrato do nuget

---
+++ GeekBurger.LabelLoader.Contract/ILabelImageAddedOut.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace GeekBurger.LabelLoader.Contract
+{
+    /// <summary>
+    /// Defines the OUT contract
+    /// </summary>
+    interface ILabelImageAddedOut
+    {
+        /// <summary>
+        /// Gets or sets the product name
+        /// </summary>
+        string ProductName { get; set; }
+        /// <summary>
+        /// Gets or sets the list of ingredients
+        /// </summary>
+        IEnumerable<string> Ingredients { get; set; }
+    }
+}
