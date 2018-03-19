ae9c9b974368fc938a648b768cdb7d22ccb30c0e
Wagner Davanço <wagnerdavanco@hotmail.com>
2018-03-19 20:31:34 -0300

    connection string blobstorage

--- GeekBurger.LabelLoader/FunctionLabelLoader.cs
+++ GeekBurger.LabelLoader/FunctionLabelLoader.cs
@@ -18,7 +18,7 @@ namespace GeekBurger.LabelLoader
         const string QueueName = "LabelLoader";
 
         [FunctionName("FunctionLabelloader ")]
-        public async static void Run([BlobTrigger("LabelImage/{name}", Connection = "")]Stream myBlob, string name, TraceWriter log)
+        public async static void Run([BlobTrigger("LabelImage/{name}", Connection = "DefaultEndpointsProtocol=https;AccountName=labelloaderproductimages;AccountKey=nrjPqOqdt3DRA5vj69qkcEOkeIYI64hnkuYfRvfo9LsBFZTanLoRt9gCYDQx7K2k9fxPao4+OagInG67Pdbt+Q==;EndpointSuffix=core.windows.net")]Stream myBlob, string name, TraceWriter log)
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
