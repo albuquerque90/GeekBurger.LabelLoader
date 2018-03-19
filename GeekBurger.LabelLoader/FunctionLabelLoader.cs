8d8ac1874fc47d6901f43d0a7aec328fcb9a4ccb
Wagner Davanço <wagnerdavanco@hotmail.com>
2018-03-19 19:48:08 -0300

    add function labellolder

---
+++ GeekBurger.LabelLoader/FunctionLabelLoader.cs
@@ -0,0 +1,42 @@
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.ProjectOxford.Vision;
+using System.Text;
+
+namespace GeekBurger.LabelLoader
+{
+    public static class FunctionLabelLoader
+    {
+        private static IConfiguration _configuration;
+        private static ServiceBusConfiguration serviceBusConfiguration;
+
+        const string VisionApiKey = "acff22f5f9b541c8a6849e4239c66744";
+        const string QueueName = "<your_queue_name>";
+
+        [FunctionName("FunctionLabelloader ")]
+        public async static void Run([BlobTrigger("LabelImage/{name}", Connection = "")]Stream myBlob, string name, TraceWriter log)
+        {
+            log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            serviceBusConfiguration = _configuration.GetSection("serviceBus").Get<ServiceBusConfiguration>();
+
+            var client = new VisionServiceClient(VisionApiKey);
+
+            var result = await client.RecognizeTextAsync(myBlob, languageCode: "pt", detectOrientation: true);
+
+            var queueClient =  new QueueClient(serviceBusConfiguration.ConnectionString, QueueName);
+
+            var jsonString = JsonConvert.SerializeObject(result);
+
+            var message = new Message(Encoding.UTF8.GetBytes(jsonString));
+
+            await queueClient.SendAsync(message);
+
+            await queueClient.CloseAsync();
+        }
+    }
+}
